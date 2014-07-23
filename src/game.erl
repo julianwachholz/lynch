e389f8b851b75659ef03a563ee6ca2eadbb1a1c0
Julian Wachholz <julian@wachholz.ch>
2014-07-23 10:28:06 +0200

    Scaffold the game fsm.

--- src/game.erl
+++ src/game.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_fsm).
 
 %% API
--export([start_link/1]).
+-export([start/1, start_link/1]).
 -export([join/3, rename/3, talk/3]).
 
 %% gen_fsm
@@ -13,8 +13,11 @@
 
 %%% API
 
+start(Name) ->
+    gen_fsm:start(?MODULE, [Name], []).
+
 start_link(Name) ->
-    gen_fsm:start_link({global, Name}, ?MODULE, [Name], []).
+    gen_fsm:start_link(?MODULE, [Name], []).
 
 
 join(PlayerPid, PlayerState, Data) ->
@@ -36,6 +39,7 @@ init(Name) ->
                players => [], },
     {ok, lobby, State}.
 
+
 lobby({join, PlayerPid, PlayerState, Data}, State) ->
     Players = [PlayerPid | maps:get(players, State)],
     NewState = State#{ players := Players },
@@ -47,3 +51,25 @@ lobby(_Event, State) ->
 
 lobby(_Event, _From, State) ->
     {next_state, lobby, State}.
+
+
+% All state events
+
+handle_event(_Event, StateName, State) ->
+    {next_state, StateName, State}.
+
+
+handle_sync_event(_Event, _From, StateName, State) ->
+    {next_state, StateName, State}.
+
+
+handle_info(_Info, StateName, State) ->
+    {next_state, StateName, State}.
+
+
+terminate(normal, _StateName, _State) ->
+    ok.
+
+
+code_change(_OldVsn, StateName, State, _Extra) ->
+    {ok, StateName, State}.
